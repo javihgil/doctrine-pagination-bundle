1467b338861562d8406e919b57052a5005d1fcdd
Javier Hernández Gil <javibilboweb@gmail.com>
2015-12-13 18:46:47 +0100

    Base files

---
+++ JhgPaginationBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Jhg\DoctrinePaginationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class JhgPaginationBundle
+ */
+class JhgPaginationBundle extends Bundle
+{
+
+} 
