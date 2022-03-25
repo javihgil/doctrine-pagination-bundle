877d430eb96bdcd989f34ea77dea5c1877cb8720
Javi H. Gil <javihgil@softspring.es>
2022-03-25 10:38:16 +0100

    Update directory structure

--- src/DependencyInjection/JhgPaginationExtension.php
+++ src/DependencyInjection/JhgPaginationExtension.php
@@ -4,14 +4,14 @@ namespace Jhg\DoctrinePaginationBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 class JhgPaginationExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yml');
     }
 }
--- src/JhgPaginationBundle.php
+++ src/JhgPaginationBundle.php
@@ -5,5 +5,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class JhgPaginationBundle extends Bundle
 {
-
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 } 
